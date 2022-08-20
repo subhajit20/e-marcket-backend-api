ada8e46040a3a973b6d4ca81ce76c0f9c2d4ea9e
Subhajit Ghosh <subhajitstudent8@gmail.com>
2022-08-20 08:37:26 +0530

    issue fixed

--- src/app.js
+++ src/app.js
@@ -25,7 +25,10 @@ const UserRouter = require('../routes/User.route');
  * Connecting to the mongoDB atlash database
  */
 mongoose.connect(process.env.MONGO_URI, {
-
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 }).then(() => {
     console.log("Database has been connected successfully...");
 }).catch(() => {
